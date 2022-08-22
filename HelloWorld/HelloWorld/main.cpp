c7f9118bf4d531db17625f396612a49aaf631f9c
j2695203 <j2695203@gmail.com>
2022-08-23 02:18:21 +0800

    committing my hello world project to git

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Jinny on 2022/8/23.
+//
+
+#include <iostream> //def?
+
+int main(int argc, const char * argv[]) { //def
+    // insert code here...
+    std::cout << "Hello, MSD2022!\n"; //statment
+    std::cout << 2+2;
+    std::cout << "\n";
+    return 0;
+}
