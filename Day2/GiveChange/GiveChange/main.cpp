729a4b0c581d831314b6a39be448390c705de514
j2695203 <j2695203@gmail.com>
2022-08-24 02:30:52 +0800

    just try commit a folder

---
+++ Day2/GiveChange/GiveChange/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GiveChange
+//
+//  Created by Jinny on 2022/8/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
