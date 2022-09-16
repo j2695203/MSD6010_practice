3fccc61313d4fa03b82cdd31a5428cdbf535fac5
Jinny Jeng <j2695203@gmail.com>
2022-09-16 15:50:19 -0600

    type template

---
+++ Day17/DiyVector/DiyVector/MyFunc.cpp
@@ -0,0 +1,10 @@
+//
+//  MyFunc.cpp
+//  DiyVector
+//
+//  Created by Jinny Jeng on 9/13/22.
+//
+
+#include "MyFunc.hpp"
+#include <iostream>
+
---
+++ Day17/DiyVector/DiyVector/testFunc.cpp
@@ -0,0 +1,8 @@
+//
+//  testFunc.cpp
+//  DiyVector
+//
+//  Created by Jinny Jeng on 9/16/22.
+//
+
+#include "testFunc.hpp"
